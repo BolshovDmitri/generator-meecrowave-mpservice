312d5b79eb22dcc064cbf53b49c21e9183d87986
Demis Gallisto <diuis@yahoo.it>
2020-01-27 22:17:32 +0100

    add test dir

--- generators/app/index.js
+++ generators/app/index.js
@@ -55,11 +55,23 @@ module.exports = class extends Generator {
   }
 
   templates() {
+    this.readme()
+    this.docker()
+    this.gitignore()
+    this.github()
+    this.maven()
+    this.javaBe()
+  }
+
+  readme() {
     this.fs.copyTpl(
       this.templatePath("README.md"),
       this.destinationPath("README.md"),
       {}
     );
+  }
+
+  docker() {
     this.fs.copyTpl(
       this.templatePath("docker-compose.yml"),
       this.destinationPath("docker-compose.yml"),
@@ -69,11 +81,17 @@ module.exports = class extends Generator {
         package: this.answers.packageDomain
       }
     );
+  }
+
+  gitignore() {
     this.fs.copyTpl(
       this.templatePath("gitignore"),
       this.destinationPath(".gitignore"),
       {}
     );
+  }
+
+  github() {
     this.fs.copyTpl(
       this.templatePath("github/workflows/settings.xml"),
       this.destinationPath(".github/workflows/settings.xml"),
@@ -89,14 +107,52 @@ module.exports = class extends Generator {
       this.destinationPath(".dependabot/config.yml"),
       { gitAuth: this.answers.gitAuth }
     );
+  }
+
+  maven() {
     this.fs.copyTpl(
       this.templatePath("maven/jvm.config"),
       this.destinationPath(".mvn/jvm.config"),
       {}
     );
+  }
+
+  pathBe() {
+    return this.answers.name + "-multi-module/" + this.answers.name + "-be"
+  }
+
+  pathPackageBe() {
+    return this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/be"
+  }
+
+  pathAdapter() {
+    return this.answers.name + "-multi-module/" + this.answers.name + "-adapter"
+  }
+
+  pathPackageAdapter() {
+    return this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/adapter"
+  }
+
+  pathDomain() {
+    return this.answers.name + "-multi-module/" + this.answers.name + "-domain"
+  }
+
+  pathPackageDomain() {
+    return this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/domain"
+  }
+
+  pathSwagger() {
+    return this.answers.name + "-multi-module/" + this.answers.name + "-swagger"
+  }
+
+  pathPackageSwagger() {
+    return this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/swagger"
+  }
+
+  javaBe() {
     this.fs.copyTpl(
       this.templatePath("java/be/Application"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/java/" + this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/be/meecrowave/" + "Application.java"),
+      this.destinationPath(this.pathBe() + "/src/main/java/" + this.pathPackageBe() + "/meecrowave/" + "Application.java"),
       {
         packageDomain: this.answers.packageDomain,
         packageModule: this.answers.name.split("-").join(".").toLowerCase() + ".be"
@@ -104,35 +160,64 @@ module.exports = class extends Generator {
     );
     this.fs.copyTpl(
       this.templatePath("java/be/Api"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/java/" + this.answers.packageDomain.split(".").join("/").toLowerCase() + "/" + this.answers.name.split("-").join("/").toLowerCase() + "/be/meecrowave/" + "Api.java"),
+      this.destinationPath(this.pathBe() + "/src/main/java/" + this.pathPackageBe() + "/meecrowave/" + "Api.java"),
       {
         packageDomain: this.answers.packageDomain,
         packageModule: this.answers.name.split("-").join(".").toLowerCase() + ".be"
       }
     );
     this.fs.copyTpl(
+      this.templatePath("gitkeep"),
+      this.destinationPath(this.pathBe() + "/src/test/java/" + this.pathPackageBe() + "/meecrowave/" + ".gitkeep"),
+      {}
+    );
+    this.fs.copyTpl(
       this.templatePath("java/be/meta-inf/microprofile-config.properties"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/resources/META-INF/microprofile-config.properties"),
+      this.destinationPath(this.pathBe() + "/src/main/resources/META-INF/microprofile-config.properties"),
       {}
     );
     this.fs.copyTpl(
       this.templatePath("java/be/meecrowave/meecrowave.properties"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/resources/meecrowave.properties"),
+      this.destinationPath(this.pathBe() + "/src/main/resources/meecrowave.properties"),
       {}
     );
     this.fs.copyTpl(
       this.templatePath("java/be/log4j2/log4j2-dev.xml"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/resources/log4j2-dev.xml"),
+      this.destinationPath(this.pathBe() + "/src/main/resources/log4j2-dev.xml"),
       {}
     );
     this.fs.copyTpl(
       this.templatePath("java/be/log4j2/log4j2.xml"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/src/main/resources/log4j2.xml"),
+      this.destinationPath(this.pathBe() + "/src/main/resources/log4j2.xml"),
       {}
     );
     this.fs.copyTpl(
       this.templatePath("java/be/log4j2/empty-log.txt"),
-      this.destinationPath(this.answers.name + "-multi-module/" + this.answers.name + "-be" + "/logs/app.log"),
+      this.destinationPath(this.pathBe() + "/logs/app.log"),
+      {}
+    );
+  }
+
+  javaAdapter() {
+    this.fs.copyTpl(
+      this.templatePath("gitkeep"),
+      this.destinationPath(this.pathAdapter() + "/src/test/java/" + this.pathPackageBe() + "/" + ".gitkeep"),
+      {}
+    );
+  }
+
+  javaDomain() {
+    this.fs.copyTpl(
+      this.templatePath("gitkeep"),
+      this.destinationPath(this.pathDomain() + "/src/test/java/" + this.pathPackageDomain() + "/" + ".gitkeep"),
+      {}
+    );
+  }
+
+  javaSwagger() {
+    this.fs.copyTpl(
+      this.templatePath("gitkeep"),
+      this.destinationPath(this.pathSwagger() + "/src/test/java/" + this.pathPackageSwagger() + "/" + ".gitkeep"),
       {}
     );
   }
